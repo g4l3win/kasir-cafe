61baee63f28936133fbfbf1b964d2c4336df2d04
Gabyhere <135355709+Gabyhere@users.noreply.github.com>
2023-09-11 20:52:49 +0700

    Delete Form1.vb

--- Form1.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class Form1
-    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
-
-    End Sub
-
-    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs)
-
-    End Sub
-End Class
