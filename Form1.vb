c63777e7a3a540ed4469e4c42ee699a7b5c377ec
Gabyhere <135355709+Gabyhere@users.noreply.github.com>
2023-09-11 20:53:44 +0700

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show("Data telah tersimpan!.")
+    End Sub
+
+    Private Sub TextBox5_TextChanged(sender As Object, e As EventArgs) Handles TextBox5.TextChanged
+
+    End Sub
+
+    Private Sub Label7_Click(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
